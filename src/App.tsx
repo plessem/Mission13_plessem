c6859446ea3b4d7f1c359fe4b95b59b98df77478
Emma Plessinger <plessinger.em@gmail.com>
2023-03-31 23:00:19 -0600

    movie list on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TopBanner from './Home';
+import MovieList from './Movies';
 
 function App() {
   return (
     <div className="App">
       <TopBanner />
+      <MovieList />
     </div>
   );
 }
